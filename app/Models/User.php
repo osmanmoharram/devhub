568177d26294396ae51932cc0fabf2e7ac0f1a36
Osman Moharram <o.moharram91@gmail.com>
2026-02-08 02:14:39 +0300

    remove psalm package

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
@@ -27,7 +27,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<array-key, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
