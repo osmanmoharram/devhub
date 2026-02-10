5a8145fde62c7abec6bd6bccaa8fd6926205a924
Osman Moharram <o.moharram91@gmail.com>
2026-02-10 09:10:36 +0300

    Feature: Profile Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -37,6 +38,14 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+    /**
+     * @return HasOne<Profile, $this>
+     */
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
     /**
      * @return HasMany<Reply, $this>
      */
