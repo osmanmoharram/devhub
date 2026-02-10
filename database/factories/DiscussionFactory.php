4c5855aeb0be64886b196de63e36750515dd7127
Osman Moharram <o.moharram91@gmail.com>
2026-02-10 08:43:22 +0300

    fix code style for DiscussionFactory

--- database/factories/DiscussionFactory.php
+++ database/factories/DiscussionFactory.php
@@ -19,7 +19,7 @@ class DiscussionFactory extends Factory
         return [
             'title' => $title,
             'slug' => Str::slug($title),
-            'content' =>  fake()->paragraphs(3, true),
+            'content' => fake()->paragraphs(3, true),
             'category_id' => Category::factory(),
             'user_id' => User::factory(),
             'is_pinned' => fake()->boolean(10), // 10% chance of being pinned
